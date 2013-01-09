0a3c11f0c769b4086583a464a97663981136ec93
Gijs Nijholt <gijs.nijholt@gmail.com>
2013-01-09 10:45:25 +0100

    ...

---
+++ scripts/lizard.home.js
@@ -0,0 +1,37 @@
+Lizard.Home = {};
+
+Lizard.Home.DefaultView = Backbone.Marionette.ItemView.extend({
+  template: '#home-template',
+  className: 'home',
+  onShow: function() {
+    console.log('onShow()');
+    $('.search-query').focus();
+  },
+  onDomRefresh: function() {
+    console.log('onDomRefresh');
+  }
+});
+
+Lizard.Home.Router = Backbone.Marionette.AppRouter.extend({
+    appRoutes: {
+      '': 'home',
+      'home': 'home'
+    }
+});
+
+Lizard.Home.home = function(){
+  console.log('Lizard.Home.home()');
+  
+  var homeView = new Lizard.Home.DefaultView();
+
+  Lizard.content.show(homeView);
+  Backbone.history.navigate('home');
+};
+
+Lizard.addInitializer(function(){
+  Lizard.Home.router = new Lizard.Home.Router({
+    controller: Lizard.Home
+  });
+  
+  Lizard.vent.trigger('routing:started');
+});
