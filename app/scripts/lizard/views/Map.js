18d1dbabe7e3ba226effc4009a70f8f6bf056224
Roland van Laar <roland@micite.net>
2014-01-05 19:00:55 +0100

    Update Waterkaart basemap to use the new tile server.

--- app/scripts/lizard/views/Map.js
+++ app/scripts/lizard/views/Map.js
@@ -23,14 +23,14 @@ Lizard.Views.Map = Backbone.Marionette.ItemView.extend({
     this.workspace = options.workspace;
     this.backgroundLayers = {
       // Satellite :new L.Google("SATELLITE", {detectRetina: true}),
-      Waterkaart: L.tileLayer.wms("http://test.deltaportaal.lizardsystem.nl/service/", {
-        layers: 'deltaportaal',
-        format: 'image/png',
-        transparent: true,
+	  Waterkaart: = L.tileLayer("https://tiles.lizard.net/tms/1.0.0/basiskaart/webmercator/{z}/{x}/{y}.png", {
+        transparant: true,
         reuseTiles: true,
-        attribution: "Dijkdata",
-        maxZoom: 30
+        attribution: "Deltaportaal",
+		zoomOffset: -1,
+		tms: true
       }),
+
       OpenStreetMap: new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data © OpenStreetMap contributors'
       })
